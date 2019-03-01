94b646e23434560781609972f36fab77025491e1
Ursula Neumann <ursulahneumann@gmail.com>
2019-02-28 16:05:52 -0800

    Update plots according to data files

--- wrangling_scripts/wrangle_data.py
+++ wrangling_scripts/wrangle_data.py
@@ -4,6 +4,37 @@ import plotly.graph_objs as go
 # Use this file to read in your data and prepare the plotly visualizations. The path to the data files are in
 # `data/file_name.csv`
 
+def cleandata(dataset, keepcolumns = ['Country', '1980', '2008'], value_variables = ['1980', '2008']):
+    """Clean Gapminder cholesterol and BMI data for a visualizaiton dashboard
+
+    Keeps data range of dates in keep_columns variable and data for the top 10 economies
+    Reorients the columns into a year, country and value
+    Saves the results to a csv file
+
+    Args:
+        dataset (str): name of the csv data file
+
+    Returns:
+        None
+
+    """
+    df = pd.read_csv(dataset)
+
+    # Keep only the columns of interest (years and country name)
+    df = df[keepcolumns]
+
+    top10country = ['United States', 'China', 'Japan', 'Germany', 'United Kingdom', 'India', 'France', 'Brazil', 'Italy', 'Canada']
+    df = df[df['Country'].isin(top10country)]
+
+    # melt year columns  and convert year to date time
+    df_melt = df.melt(id_vars='Country', value_vars = value_variables)
+    df_melt.columns = ['country','year', 'variable']
+    df_melt['year'] = df_melt['year'].astype('datetime64[ns]').dt.year
+
+    # output clean csv file
+    return df_melt
+
+
 def return_figures():
     """Creates four plotly visualizations
 
@@ -14,69 +45,77 @@ def return_figures():
         list (dict): list containing the four plotly visualizations
 
     """
+    df_chol = cleandata('data/female_cholesterol.csv')
+    df_BMI = cleandata('data/female_BMI.csv')
 
-    # first chart plots arable land from 1990 to 2015 in top 10 economies
+    # first chart plots cholesterol levels from 1980 to 2008 in top 10 economies
     # as a line chart
-
     graph_one = []
-    graph_one.append(
-      go.Scatter(
-      x = [0, 1, 2, 3, 4, 5],
-      y = [0, 2, 4, 6, 8, 10],
-      mode = 'lines'
-      )
-    )
+    df_chol.columns = ['country','year','cholesterol']
+    countrylist = df_chol.country.unique().tolist()
 
-    layout_one = dict(title = 'Chart One',
-                xaxis = dict(title = 'x-axis label'),
-                yaxis = dict(title = 'y-axis label'),
+    for country in countrylist:
+      x_val = df_chol[df_chol['country'] == country].year.tolist()
+      y_val =  df_chol[df_chol['country'] == country].cholesterol.tolist()
+      graph_one.append(
+          go.Scatter(
+          x = x_val,
+          y = y_val,
+          mode = 'lines',
+          name = country
+          )
+      )
+
+    layout_one = dict(title = 'Cholesterol Levels Over Time',
+                xaxis = dict(title = 'Year'),
+                yaxis = dict(title = 'Total Cholesterol (mmol/L)'),
                 )
 
-# second chart plots ararble land for 2015 as a bar chart
+# second chart plots BMI from 1980 to 2008 in a line chart
     graph_two = []
+    df_BMI.columns = ['country','year','BMI']
+    #df_BMI.sort_values('BMI', ascending=False, inplace=True)
+    countrylist = df_BMI.country.unique().tolist()
 
-    graph_two.append(
-      go.Bar(
-      x = ['a', 'b', 'c', 'd', 'e'],
-      y = [12, 9, 7, 5, 1],
+    for country in countrylist:
+      x_val = df_BMI[df_BMI['country'] == country].year.tolist()
+      y_val =  df_BMI[df_BMI['country'] == country].BMI.tolist()
+      graph_two.append(
+          go.Scatter(
+          x = x_val,
+          y = y_val,
+          mode = 'lines',
+          name = country
+          )
       )
-    )
 
-    layout_two = dict(title = 'Chart Two',
-                xaxis = dict(title = 'x-axis label',),
-                yaxis = dict(title = 'y-axis label'),
+    layout_two = dict(title = 'BMI Levels Over Time',
+                xaxis = dict(title = 'Year'),
+                yaxis = dict(title = 'BMI (kg/m^2)'),
                 )
 
 
-# third chart plots percent of population that is rural from 1990 to 2015
+# third chart plots cholesterol vs BMI as a scatterplot
     graph_three = []
-    graph_three.append(
-      go.Scatter(
-      x = [5, 4, 3, 2, 1, 0],
-      y = [0, 2, 4, 6, 8, 10],
-      mode = 'lines'
+    df_BMI.columns = ['country','year','BMI']
+    #df_BMI.sort_values('BMI', ascending=False, inplace=True)
+    countrylist = df_BMI.country.unique().tolist()
+
+    for country in countrylist:
+      x_val = df_BMI[df_BMI['country'] == country].BMI.tolist()
+      y_val =  df_chol[df_chol['country'] == country].cholesterol.tolist()
+      graph_three.append(
+          go.Scatter(
+          x = x_val,
+          y = y_val,
+          mode = 'markers',
+          name = country
+          )
       )
-    )
 
-    layout_three = dict(title = 'Chart Three',
-                xaxis = dict(title = 'x-axis label'),
-                yaxis = dict(title = 'y-axis label')
-                       )
-
-# fourth chart shows rural population vs arable land
-    graph_four = []
-
-    graph_four.append(
-      go.Scatter(
-      x = [20, 40, 60, 80],
-      y = [10, 20, 30, 40],
-      mode = 'markers'
-      )
-    )
-
-    layout_four = dict(title = 'Chart Four',
-                xaxis = dict(title = 'x-axis label'),
-                yaxis = dict(title = 'y-axis label'),
+    layout_three = dict(title = 'Cholesterol vs BMI Levels',
+                xaxis = dict(title = 'Cholesterol (mmol/dL)'),
+                yaxis = dict(title = 'BMI (kg/m^2)'),
                 )
 
     # append all charts to the figures list
@@ -84,6 +123,5 @@ def return_figures():
     figures.append(dict(data=graph_one, layout=layout_one))
     figures.append(dict(data=graph_two, layout=layout_two))
     figures.append(dict(data=graph_three, layout=layout_three))
-    figures.append(dict(data=graph_four, layout=layout_four))
 
     return figures
